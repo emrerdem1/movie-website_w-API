c7e31a52d9a96148a510c5f9329ebeb2e5dde6ab
emrerdem1 <emrerdem94@gmail.com>
2020-09-01 14:43:33 +0300

    Clear: clear file tree

--- anime-comments/src/components/layout/CustomBackground.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./CustomBackground.scss";
-
-const CustomBackground = (): JSX.Element => {
-  return <div className="custom-background"></div>;
-};
-
-export default CustomBackground;
